e5c7f11f48735d9e8930a77d10102f5f3d518ace
Ruben Duran <rd@rubenduran.net>
2018-02-05 22:18:13 -0700

    added email route and view

--- app.js
+++ app.js
@@ -22,6 +22,50 @@ app.post('/result', function(req, res) {
 
  res.render("results", {myData:myData});
 })
+
+app.post('/email', function(req, res) {
+ console.log("POST DATA", req.body);
+ var myData = req.body;
+
+ nodemailer.createTestAccount((err, account) => {
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass  // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"OCEO TEST" <foo@example.com>', // sender address
+        to: 'foo@example.com', // list of receivers
+        subject: 'Hello', // Subject line
+        text: 'Hello', // plain text body
+        html: '<b>Hello world</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+});
+
+
+ res.render("thanks", {myData:myData});
+})
 // tell the express app to listen on port 8000
 app.listen(8000, function() {
  console.log("listening on port 8000");
